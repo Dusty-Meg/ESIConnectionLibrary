de70902a9f6dec39a504ea385a156585d12ae011
Matthew Moore <matthewemmoore@googlemail.com>
2018-06-08 23:26:18 +0100

    Polly policies should now retry using proper exception

--- ESIConnectionLibrary/ESIConnectionLibrary/Internal classes/PollyPolicies.cs
+++ ESIConnectionLibrary/ESIConnectionLibrary/Internal classes/PollyPolicies.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Net;
+using ESIConnectionLibrary.Exceptions;
 using Polly;
 
 namespace ESIConnectionLibrary.Internal_classes
 {
     internal class PollyPolicies
     {
-        public static readonly Policy WebExceptionRetryWithFallbackAsync = Policy.Handle<WebException>().WaitAndRetryAsync(new[]
+        public static readonly Policy WebExceptionRetryWithFallbackAsync = Policy.Handle<ESIException>().WaitAndRetryAsync(new[]
         {
             TimeSpan.FromSeconds(1),
             TimeSpan.FromSeconds(2),
             TimeSpan.FromSeconds(3)
         });
 
-        public static readonly Policy WebExceptionRetryWithFallback = Policy.Handle<WebException>().WaitAndRetry(new[]
+        public static readonly Policy WebExceptionRetryWithFallback = Policy.Handle<ESIException>().WaitAndRetry(new[]
         {
             TimeSpan.FromSeconds(1),
             TimeSpan.FromSeconds(2),
